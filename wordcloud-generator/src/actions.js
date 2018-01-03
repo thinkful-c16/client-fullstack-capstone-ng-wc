49578558d78bf4bacb3292cd86f0d6396df7e0c4
Nicole Gilbert <nicolegilbert16@gmail.com>
2018-01-03 09:26:41 -0800

    added actions + to reducer

--- wordcloud-generator/src/actions.js
+++ wordcloud-generator/src/actions.js
@@ -1,5 +1,22 @@
-export const GENERATE_WORDCLOUD = 'GENERATE_WORDCLOUD';
-export const generateWordCloud = () => ({
-  type: GENERATE_WORDCLOUD,
+import {API_BASE_URL} from '../config';
+
+export const GENERATE_CLOUD = 'GENERATE_CLOUD';
+export const generateCloud = () => ({
+  type: GENERATE_CLOUD,
+  title,
   words,
+  font,
+  color
 });
+
+export const FETCH_CLOUDS = 'FETCH_CLOUDS';
+export const fetchClouds = () => ({
+  type: FETCH_CLOUDS,
+  clouds
+})
+
+export const FETCH_SINGLE_CLOUD = 'FETCH_SINGLE_CLOUD';
+export const fetchSingleCloud = () => ({
+  type: FETCH_SINGLE_CLOUD,
+  activeCloud
+})
--- wordcloud-generator/src/reducer.js
+++ wordcloud-generator/src/reducer.js
@@ -1,12 +1,30 @@
-import {  } from './actions';
+import { 
+  GENERATE_CLOUD,
+  FETCH_CLOUDS,
+  FETCH_SINGLE_CLOUD
+  } from './actions';
 
 const initialState = {
   view: 'home',
-  activeWordCloud: {},
-  font: '',
-  fontColor: ''
+  clouds: [],
+  activeCloud: {}
 };
 
-export const reducer = (state =initialState, action) => {
+export const reducer = (state = initialState, action) => {
+  if(action.type === GENERATE_CLOUD){
+    return Object.assign({}, state, {
+      view: 'newCloud'
+    })
+  }
+  if(action.type === FETCH_CLOUDS){
+    return Object.assign({}, state, {
+      view: 'library'
+    })
+  }
+  if(action.type === FETCH_SINGLE_CLOUD){
+    return Object.assign({}, state, {
+      view: 'focus'
+    })
+  }
   return state;
 };
