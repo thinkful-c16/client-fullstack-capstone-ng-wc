59fa4c7c51f9506de4a1a7f8f4d449d334554442
Nicole Gilbert <nicolegilbert16@gmail.com>
2018-01-05 13:33:48 -0800

    added the netlify file

--- wordcloud-generator/src/config.js
+++ wordcloud-generator/src/config.js
@@ -1,6 +1,8 @@
+'use strict';
+
 module.exports = {
   API_BASE_URL:
-        process.env.REACT_APP_API_BASE_URL=  'https://git.heroku.com/wordcloud-generator.git'
+        process.env.REACT_APP_API_BASE_URL=  'https://wordcloud-generator.herokuapp.com/'
         || 'http://localhost:8080',
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
