910076c7916cc3af6d7f99fd5fc15bce578aea47
Nicole Gilbert <nicolegilbert16@gmail.com>
2018-01-05 14:22:31 -0800

    updated api base url to get it working

--- wordcloud-generator/src/config.js
+++ wordcloud-generator/src/config.js
@@ -2,13 +2,10 @@
 
 module.exports = {
   API_BASE_URL:
-        process.env.REACT_APP_API_BASE_URL=  'https://wordcloud-generator.herokuapp.com/'
-        || 'http://localhost:8080',
+        process.env.REACT_APP_API_BASE_URL='http://localhost:8080',
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-//   DATABASE_URL:
-//         process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
-//   TEST_DATABASE_URL:
-//         process.env.TEST_DATABASE_URL ||
-//         'mongodb://localhost/thinkful-backend-test'
+
+  // process.env.REACT_APP_API_BASE_URL=  'https://wordcloud-generator.herokuapp.com/'
+  // || 'http://localhost:8080',
 };
